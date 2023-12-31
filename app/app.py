2323d48fe3a9fdb9a436087a87f1a56654afc700
buarki <oreasekadd@gmail.com>
2023-12-31 00:07:27 -0300

    chore: set supported files (#20)

--- app/app.py
+++ app/app.py
@@ -4,7 +4,13 @@ from predict_image import predict
 from constants import MB
 
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 1 * MB
+app.config['MAX_CONTENT_LENGTH'] = 2 * MB
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+# Function to check if a filename has an allowed extension
+def allowed_file(filename):
+  return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/predict-result', methods=['POST'])
 def predict3():
@@ -15,6 +21,11 @@ def predict3():
 
     number1_image = request.files['image1']
     number2_image = request.files['image2']
+
+    if not (allowed_file(number1_image.filename) and allowed_file(number2_image.filename)):
+      print("Invalid file type", flush=True)
+      return json.dumps({'error': 'Invalid file type. Allowed types: png, jpg, jpeg'})
+
     operation = request.form['operation']
     print("OPERATION is", operation, flush=True)
 
@@ -39,6 +50,7 @@ def home():
   return render_template('index.html')
 
 # TODO SUPPORTED FILES...
+
 # TODO RATE LIMITING
 # TODO LOGGING...
 # TODO TRACING...
